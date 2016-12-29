4715c96e3c99b796286f7dc5a673a47424a55b7e
asenthil2k <asenthilkumar@Senthils-MacBook-Pro.local>
2016-12-28 19:04:21 -0500

    Second Checkin

--- js/file1.js
+++ js/file1.js
@@ -5,20 +5,21 @@ var app1 = angular.module('app1', []);
 
 app1.controller('cntrl1', function($scope) {
 
-   $scope.first = 1;
-   $scope.second = 2 ;
+   $scope.first = 'Senthil';
+   $scope.second = 'Kumar' ;
 
    $scope.calculate = function() {
 
      $scope.third = $scope.first + $scope.second;
+
    }
 
 });
 
 app1.controller('cntrl2', function($scope) {
 
-   $scope.first = 1;
-   $scope.second = 2 ;
+   $scope.firstvalue = 1;
+   $scope.secondvalue = 2 ;
 
    $scope.calculate = function() {
 
